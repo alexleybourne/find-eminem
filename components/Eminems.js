e0db6610680c65467c3f6a657c330bfde2a96960
alexleybourne <alexleybourne1@gmail.com>
2020-10-25 15:57:57 +1000

    Added random colour logic

--- components/Eminems.js
+++ components/Eminems.js
@@ -1,16 +1,26 @@
+import React, { useState } from 'react';
 
 const Eminems = () => {
+
+  const [colour, setColour] = useState('Green');
 
   const found = () => {
     console.log('FOUND EMINEM')
   }
 
+  const setup = () => {
+    const colours = ['Green', 'Red', 'Orange', 'Blue']
+    const randomColour = colours[~~(Math.random() * colours.length)]
+    setColour(randomColour)
+    console.log(randomColour)
+  }
+
   return (
-    <div className="main-image">
+    <div className="main-image" onLoad={setup}>
       <div className="main-image-wrapper">
         <div id="eminem-wrapper" onClick={found}>
           <img id="eminem" src="assets/Eminem.png" alt="Eminem"/>
-          <img id="eminem-bg" src="assets/Green.png" alt="Eminem M&M" />
+          <img id="eminem-bg" src={`assets/${colour}.png`} alt="Eminem M&M" />
         </div>
         <img className="background" src="/assets/Background.jpg" alt="M&M's"/>
       </div>
